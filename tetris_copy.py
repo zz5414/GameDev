7f879d15cd8f1e3cdc8aa3409483c4e113b4f022
heejin.park <zzqq5414@gmail.com>
2022-03-05 03:10:47 +0900

    pyqt tetris 완성

--- tetris_copy.py
+++ tetris_copy.py
@@ -32,8 +32,8 @@ class Tetris(QMainWindow):
         self.tboard = Board(self)
         self.setCentralWidget(self.tboard)
 
-        # self.statusbar = self.statusBar()
-        # self.tboard.msg2Statusbar[str].connect(self.statusbar.showMessage)
+        self.statusbar = self.statusBar()
+        self.tboard.msg2Statusbar[str].connect(self.statusbar.showMessage)
 
         self.tboard.start()
 
@@ -52,17 +52,16 @@ class Tetris(QMainWindow):
 
 
 class Board(QFrame):
-    # msg2Statusbar = pyqtSignal(str)
+    msg2Statusbar = pyqtSignal(str)
 
-    # BoardWidth = 10
-    # BoardHeight = 22
-    BoardWidth = 5
-    BoardHeight = 4
-    Speed = 2000
+    BoardWidth = 10
+    BoardHeight = 22
+    # BoardWidth = 5
+    # BoardHeight = 10
+    Speed = 1000
 
     def __init__(self, parent):
         super().__init__(parent)
-
         self.initBoard()
 
     def initBoard(self):
@@ -74,23 +73,22 @@ class Board(QFrame):
         self.curX = 0
         self.curY = 0
         self.numLinesRemoved = 0
-        self.board = []
+        self.board = None
 
         self.setFocusPolicy(Qt.StrongFocus)
         self.isStarted = False
         self.isPaused = False
         self.clearBoard()
 
-    def shapeAt(self, x, y):
+    def shapeAt(self, r, c):
         """determines shape at the board position"""
+        return self.board[r][c]
 
-        return self.board[(y * Board.BoardWidth) + x]
-#
-#     def setShapeAt(self, x, y, shape):
-#         """sets a shape at the board"""
-#
-#         self.board[(y * Board.BoardWidth) + x] = shape
-#
+    def setShapeAt(self, r, c, shape):
+        """sets a shape at the board"""
+
+        self.board[r][c] = shape
+
     def squareWidth(self):
         """returns the width of one square"""
 
@@ -165,38 +163,34 @@ class Board(QFrame):
         painter = QPainter(self)
         rect = self.contentsRect()
 
-        boardTop = rect.bottom() - Board.BoardHeight * self.squareHeight()
+        # boardTop = rect.bottom() - Board.BoardHeight * self.squareHeight()
 
         self.drawGrid(painter)
 
         # draw bg
-        # for i in range(Board.BoardHeight):
-        #     for j in range(Board.BoardWidth):
-        #         shape = self.shapeAt(j, Board.BoardHeight - i - 1)
-        #
-        #         if shape != Tetrominoe.NoShape:
-        #             self.drawSquare(painter,
-        #                             rect.left() + j * self.squareWidth(),
-        #                             boardTop + i * self.squareHeight(), shape)
-        #
-        # #draw current piece
-        # if self.curPiece.shape() != Tetrominoe.NoShape:
-        #     for i in range(4):
-        #         x = self.curX + self.curPiece.x(i)
-        #         y = self.curY - self.curPiece.y(i)
-        #         self.drawSquare(painter, rect.left() + x * self.squareWidth(),
-        #                         boardTop + (Board.BoardHeight - y - 1) * self.squareHeight(),
-        #                         self.curPiece.shape())
+        for r in range(Board.BoardHeight):
+            for c in range(Board.BoardWidth):
+                shape = self.shapeAt(r, c)
+
+                if shape != Tetrominoe.NoShape:
+                    self.drawSquare(painter, rect, r, c, shape)
 
 
-    def move(self, x, y):
-        if (x<0) or (x>=Board.BoardWidth) or (y<0) or (y>=Board.BoardHeight):
-            return
+        #draw current piece
+        if self.curPiece.shape() != Tetrominoe.NoShape:
+            for i in range(4):
+                c = self.curC + self.curPiece.c(i)
+                r = self.curR + self.curPiece.r(i)
+                self.drawSquare(painter, rect, r, c, self.curPiece.shape())
 
-        self.board = np.zeros((Board.BoardHeight, Board.BoardWidth))
-        self.board[(Board.BoardHeight - 1 - y)*Board.BoardWidth + x] = Tetrominoe.ZShape
-        self.curX, self.curY = x, y
-        self.update()
+
+    # def move(self, c, r):
+    #     if (c<0) or (c>=Board.BoardWidth) or (r<0) or (r>=Board.BoardHeight):
+    #         return
+    #
+    #     self.board[(Board.BoardHeight - 1 - y)*Board.BoardWidth + x] = Tetrominoe.ZShape
+    #     self.curX, self.curY = x, y
+    #     self.update()
 
     def keyPressEvent(self, event):
          """processes key press events"""
@@ -212,20 +206,24 @@ class Board(QFrame):
          key = event.key()
 
          if key == Qt.Key_Left:
-             self.move(self.curX - 1, self.curY)
-             # self.tryMove(self.curPiece, self.curX - 1, self.curY)
+             # self.move(self.curX - 1, self.curY)
+             self.tryMove(self.curPiece, self.curC - 1, self.curR)
 
          elif key == Qt.Key_Right:
-             self.move(self.curX + 1, self.curY)
+             # self.move(self.curX + 1, self.curY)
+             self.tryMove(self.curPiece, self.curC + 1, self.curR)
+
              # self.tryMove(self.curPiece, self.curX + 1, self.curY)
 
          elif key == Qt.Key_Down:
-             self.move(self.curX, self.curY+1)
+             # self.move(self.curX, self.curY+1)
+             self.tryMove(self.curPiece, self.curC, self.curR + 1)
              # self.tryMove(self.curPiece.rotateRight(), self.curX, self.curY)
 
          elif key == Qt.Key_Up:
-             self.move(self.curX, self.curY-1)
-             # self.tryMove(self.curPiece.rotateLeft(), self.curX, self.curY)
+             # self.tryMove(self.curPiece, self.curC, self.curR - 1)
+             self.tryMove(self.curPiece.rotateLeft(), self.curC, self.curR)
+
 
          # if key == Qt.Key_P:
          #     self.pause()
@@ -246,8 +244,8 @@ class Board(QFrame):
          # elif key == Qt.Key_Up:
          #     self.tryMove(self.curPiece.rotateLeft(), self.curX, self.curY)
          #
-         # elif key == Qt.Key_Space:
-         #     self.dropDown()
+         elif key == Qt.Key_Space:
+             self.dropDown()
          #
          # elif key == Qt.Key_D:
          #     self.oneLineDown()
@@ -260,138 +258,191 @@ class Board(QFrame):
         """handles timer event"""
 
         if event.timerId() == self.timer.timerId():
-            self.move(self.curX, self.curY+1)
+            # self.move(self.curC, self.curR+1)
 
-            #if self.isWaitingAfterLine:
-#                self.isWaitingAfterLine = False
-#                self.newPiece()
-#            else:
-#                self.oneLineDown()
+            if self.isWaitingAfterLine:
+               self.isWaitingAfterLine = False
+               self.newPiece()
+            else:
+               self.oneLineDown()
 
         else:
             super(Board, self).timerEvent(event)
 
     def clearBoard(self):
         """clears shapes from the board"""
+        # zero is NoShape
+        self.board = np.zeros((Board.BoardHeight, Board.BoardWidth))
 
-        for i in range(Board.BoardHeight * Board.BoardWidth):
-            self.board.append(Tetrominoe.NoShape)
+
+        # for i in range(Board.BoardHeight * Board.BoardWidth):
+        #     self.board.append(Tetrominoe.NoShape)
         # self.board[0] = Tetrominoe.ZShape
         # self.board[(Board.BoardHeight-1)*Board.BoardWidth] = Tetrominoe.ZShape
 
         # 움직일 수 있는 좌표를 생성
         # self.curX = Board.BoardWidth // 2
         # self.curY = Board.BoardHeight // 2
-        self.board[0] = Tetrominoe.ZShape
+        # self.board[0] = Tetrominoe.ZShape
 
-#
-#     def dropDown(self):
-#         """drops down a shape"""
-#
-#         newY = self.curY
-#
-#         while newY > 0:
-#
-#             if not self.tryMove(self.curPiece, self.curX, newY - 1):
-#                 break
-#
-#             newY -= 1
-#
-#         self.pieceDropped()
-#
-#     def oneLineDown(self):
-#         """goes one line down with a shape"""
-#
-#         if not self.tryMove(self.curPiece, self.curX, self.curY - 1):
-#             self.pieceDropped()
-#
-#     def pieceDropped(self):
-#         """after dropping shape, remove full lines and create new shape"""
-#
-#         for i in range(4):
-#             x = self.curX + self.curPiece.x(i)
-#             y = self.curY - self.curPiece.y(i)
-#             self.setShapeAt(x, y, self.curPiece.shape())
-#
-#         self.removeFullLines()
-#
-#         if not self.isWaitingAfterLine:
-#             self.newPiece()
-#
-#     def removeFullLines(self):
-#         """removes all full lines from the board"""
-#
-#         numFullLines = 0
-#         rowsToRemove = []
-#
-#         for i in range(Board.BoardHeight):
-#
-#             n = 0
-#             for j in range(Board.BoardWidth):
-#                 if not self.shapeAt(j, i) == Tetrominoe.NoShape:
-#                     n = n + 1
-#
-#             if n == 10:
-#                 rowsToRemove.append(i)
-#
-#         rowsToRemove.reverse()
-#
-#         for m in rowsToRemove:
-#
-#             for k in range(m, Board.BoardHeight):
-#                 for l in range(Board.BoardWidth):
-#                     self.setShapeAt(l, k, self.shapeAt(l, k + 1))
-#
-#         numFullLines = numFullLines + len(rowsToRemove)
-#
-#         if numFullLines > 0:
-#             self.numLinesRemoved = self.numLinesRemoved + numFullLines
-#             self.msg2Statusbar.emit(str(self.numLinesRemoved))
-#
-#             self.isWaitingAfterLine = True
-#             self.curPiece.setShape(Tetrominoe.NoShape)
-#             self.update()
+
+    def dropDown(self):
+        """drops down a shape"""
+
+        newR = self.curR
+
+        while newR < Board.BoardHeight:
+            if not self.tryMove(self.curPiece, self.curC, newR + 1):
+                break
+            newR += 1
+
+        self.pieceDropped()
+
+    def oneLineDown(self):
+        """goes one line down with a shape"""
+
+        if not self.tryMove(self.curPiece, self.curC, self.curR + 1):
+            self.pieceDropped()
+
+    def pieceDropped(self):
+        """after dropping shape, remove full lines and create new shape"""
+
+        for i in range(4):
+            c = self.curC + self.curPiece.c(i)
+            r = self.curR + self.curPiece.r(i)
+            self.setShapeAt(r, c, self.curPiece.shape())
+
+        self.removeFullLines()
+
+        if not self.isWaitingAfterLine:
+            self.newPiece()
+
+    # https://stackoverflow.com/questions/56700417/how-to-shift-a-2d-numpy-array-in-python
+    # num으로 입력받은 만큼 행렬을 아래로 shift
+    def shift_array(self, arr, num):
+        arr[1:num+1, :] = arr[:num, :]
+        arr[:1, :] = np.zeros(arr[0].shape)
+        return arr
+
+    def removeFullLines(self):
+        """removes all full lines from the board"""
+
+        A = np.array([[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9],
+                      [10, 11, 12]])
+
+        A[1:, :] = A[:3, :]
+        A[:1, :] = np.zeros(A[0].shape)
+
+
+        numFullLines = 0
+        for r in range(Board.BoardHeight):
+            count = len(np.where(self.board[r] > 0)[0])
+            if count == Board.BoardWidth:
+                numFullLines += 1
+                self.board = self.shift_array(self.board, r)
+
+        #
+        # numFullLines = 0
+        # rowsToRemove = []
+        #
+        # for i in range(Board.BoardHeight):
+        #
+        #     n = 0
+        #     for j in range(Board.BoardWidth):
+        #         if not self.shapeAt(j, i) == Tetrominoe.NoShape:
+        #             n = n + 1
+        #
+        #     if n == 10:
+        #         rowsToRemove.append(i)
+        #
+        # rowsToRemove.reverse()
+        #
+        # for m in rowsToRemove:
+        #
+        #     for k in range(m, Board.BoardHeight):
+        #         for l in range(Board.BoardWidth):
+        #             self.setShapeAt(l, k, self.shapeAt(l, k + 1))
+        #
+        # numFullLines = numFullLines + len(rowsToRemove)
+
+        if numFullLines > 0:
+            self.numLinesRemoved = self.numLinesRemoved + numFullLines
+            self.msg2Statusbar.emit(str(self.numLinesRemoved))
+
+            self.isWaitingAfterLine = True
+            self.curPiece.setShape(Tetrominoe.NoShape)
+            self.update()
 #
     def newPiece(self):
         """creates a new shape"""
 
         self.curPiece = Shape()
-        self.curPiece.setShape(Tetrominoe.SquareShape)
-        # self.curPiece.setRandomShape()
-        self.curX = Board.BoardWidth // 2 + 1
-        self.curY = Board.BoardHeight - 1 + self.curPiece.minY()
+        # self.curPiece.setShape(Tetrominoe.LineShape)
+        self.curPiece.setRandomShape()
+        self.curC = (Board.BoardWidth - 1) // 2
+        self.curR = 0 - self.curPiece.minR()
 
-        # if not self.tryMove(self.curPiece, self.curX, self.curY):
-        #     self.curPiece.setShape(Tetrominoe.NoShape)
-        #     self.timer.stop()
-        #     self.isStarted = False
-        #     self.msg2Statusbar.emit("Game over")
+        if not self.tryMove(self.curPiece, self.curC, self.curR):
+            self.curPiece.setShape(Tetrominoe.NoShape)
+            self.timer.stop()
+            self.isStarted = False
+            self.msg2Statusbar.emit("Game over")
 
-#     def tryMove(self, newPiece, newX, newY):
-#         """tries to move a shape"""
+
+    def tryMove(self, newPiece, newC, newR):
+        """tries to move a shape"""
+        for i in range(4):
+            c = newC + newPiece.c(i)
+            r = newR + newPiece.r(i)
+
+            # 범위를 벗어낫다면
+            if (c<0) or (c>=Board.BoardWidth) or (r<0) or (r>=Board.BoardHeight):
+                return False
+
+            # 해당 위치에 다른 무엇인가가 있을 경우
+            if self.shapeAt(r, c) != Tetrominoe.NoShape:
+                return False
+
+        self.curPiece = newPiece
+        self.curC = newC
+        self.curR = newR
+        self.update()
+
+        return True
+
+
+
+
+    # def tryMove(self, newPiece, newC, newR):
+    #     """tries to move a shape"""
+    #
+    #     for i in range(4):
+    #
+    #         x = newX + newPiece.x(i)
+    #         y = newY + newPiece.y(i)
+    #
+    #         if x < 0 or x >= Board.BoardWidth or y < 0 or y >= Board.BoardHeight:
+    #             return False
+    #
+    #         if self.shapeAt(x, y) != Tetrominoe.NoShape:
+    #             return False
+    #
+    #     self.curPiece = newPiece
+    #     self.curX = newX
+    #     self.curY = newY
+    #     self.update()
+    #
+    #     return True
 #
-#         for i in range(4):
-#
-#             x = newX + newPiece.x(i)
-#             y = newY - newPiece.y(i)
-#
-#             if x < 0 or x >= Board.BoardWidth or y < 0 or y >= Board.BoardHeight:
-#                 return False
-#
-#             if self.shapeAt(x, y) != Tetrominoe.NoShape:
-#                 return False
-#
-#         self.curPiece = newPiece
-#         self.curX = newX
-#         self.curY = newY
-#         self.update()
-#
-#         return True
-#
-    def drawSquare(self, painter, x, y, shape):
+    def drawSquare(self, painter, rect, r, c, shape):
         """draws a square of a shape"""
         color = QColor(0xCC6666)
-        painter.fillRect(x, y, self.squareWidth(), self.squareHeight(), color)
+        painter.fillRect(rect.left() + (c * self.squareWidth()),
+                         rect.top() + (r * self.squareHeight()),
+                         self.squareWidth(), self.squareHeight(),
+                         color)
 #
 #
 class Tetrominoe(object):
@@ -439,31 +490,31 @@ class Shape(object):
                 self.coords[i][j] = table[i][j]
 
         self.pieceShape = shape
-#
-#     def setRandomShape(self):
-#         """chooses a random shape"""
-#
-#         self.setShape(random.randint(1, 7))
-#
-    def x(self, index):
+
+    def setRandomShape(self):
+        """chooses a random shape"""
+
+        self.setShape(random.randint(1, 7))
+
+    def c(self, index):
         """returns x coordinate"""
 
         return self.coords[index][0]
 
-    def y(self, index):
+    def r(self, index):
         """returns y coordinate"""
 
         return self.coords[index][1]
-#
-#     def setX(self, index, x):
-#         """sets x coordinate"""
-#
-#         self.coords[index][0] = x
-#
-#     def setY(self, index, y):
-#         """sets y coordinate"""
-#
-#         self.coords[index][1] = y
+
+    def setC(self, index, c):
+        """sets x coordinate"""
+
+        self.coords[index][0] = c
+
+    def setR(self, index, r):
+        """sets y coordinate"""
+
+        self.coords[index][1] = r
 #
 #     def minX(self):
 #         """returns min x value"""
@@ -483,7 +534,7 @@ class Shape(object):
 #
 #         return m
 #
-    def minY(self):
+    def minR(self):
         """returns min y value"""
 
         m = self.coords[0][1]
@@ -501,20 +552,20 @@ class Shape(object):
 #
 #         return m
 #
-#     def rotateLeft(self):
-#         """rotates shape to the left"""
-#
-#         if self.pieceShape == Tetrominoe.SquareShape:
-#             return self
-#
-#         result = Shape()
-#         result.pieceShape = self.pieceShape
-#
-#         for i in range(4):
-#             result.setX(i, self.y(i))
-#             result.setY(i, -self.x(i))
-#
-#         return result
+    def rotateLeft(self):
+        """rotates shape to the left"""
+
+        if self.pieceShape == Tetrominoe.SquareShape:
+            return self
+
+        result = Shape()
+        result.pieceShape = self.pieceShape
+
+        for i in range(4):
+            result.setC(i, self.r(i))
+            result.setR(i, -self.c(i))
+
+        return result
 #
 #     def rotateRight(self):
 #         """rotates shape to the right"""
